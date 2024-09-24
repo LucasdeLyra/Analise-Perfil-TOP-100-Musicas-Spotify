f66a45c90a9d3207adc0486002117622f23b7821
Júlia Du Bois <jujubadubois@gmail.com>
2024-09-24 12:43:09 -0300

    Added new two-factor ANOVA

--- codigos-R/anovaDoisFatores.R
+++ codigos-R/anovaDoisFatores.R
@@ -3,26 +3,22 @@ library(readr)
 musicas <- read_csv("Documents/faculdade/Spotify_MQAM/tabelas/musicas_normalizadas.csv")
 View(musicas)
 
+# FILTRANDO OS DADOS NÃO-PARAMETRIZÁVEIS
+musicasFiltrado <- musicas[musicas$country!="NG",]
+
 # AGRUPAR OS VALORES PARA TEMPO
-musicas$tempo[musicas$tempo<0.1] <- 0
-musicas$tempo[musicas$tempo>=0.1 & musicas$tempo<0.2] <- 0.1
-musicas$tempo[musicas$tempo>=0.2 & musicas$tempo<0.3] <- 0.2
-musicas$tempo[musicas$tempo>=0.3 & musicas$tempo<0.4] <- 0.3
-musicas$tempo[musicas$tempo>=0.4 & musicas$tempo<0.5] <- 0.4
-musicas$tempo[musicas$tempo>=0.5 & musicas$tempo<0.6] <- 0.5
-musicas$tempo[musicas$tempo>=0.6 & musicas$tempo<0.7] <- 0.6
-musicas$tempo[musicas$tempo>=0.7 & musicas$tempo<0.8] <- 0.7
-musicas$tempo[musicas$tempo>=0.8 & musicas$tempo<0.9] <- 0.8
-musicas$tempo[musicas$tempo>=0.9 & musicas$tempo<1] <- 0.9
+musicasFiltrado$tempo[musicasFiltrado$tempo<=0.33] <- 0.33
+musicasFiltrado$tempo[musicasFiltrado$tempo>0.33 & musicasFiltrado$tempo<=0.67] <- 0.67
+musicasFiltrado$tempo[musicasFiltrado$tempo>0.67 & musicasFiltrado$tempo<=1] <- 1
 # musicas$tempo[musicas$tempo==1] <- 1 já está feito
 
 # DIVIDIR DATABASE EM DUAS (TESTE CRUZADO SUGERIDO PELO PROFESSOR)
-n <- 1200 # number of original database
+n <- 1100 # number of original database
 select <- sample(c(TRUE, FALSE), n, replace=TRUE, prob=c(0.5, 0.5))
-musicas1 <- musicas[select, ]
-musicas2 <- musicas[!select, ]
+musicas1 <- musicasFiltrado[select, ]
+musicas2 <- musicasFiltrado[!select, ]
 
-# ANOVA PARA MUSICAS1 (SAMPLE 1)
+# ANOVA PARA MUSICAS (FULL SAMPLE)
 # utilizamos ANOVA de dois fatores, pois temos duas variaveis independentes
 # e uma variavel dependende
 # independentes: tempo, pais
@@ -31,6 +27,10 @@ musicas2 <- musicas[!select, ]
 # para modificar variaveis:
 # dependente fica antes do '~'
 # independendetes ficam depois, separadas por '+'
+anovaMusicasFull <- aov(danceability ~ tempo + country, data = musicasFiltrado)
+summary(anovaMusicasFull)
+
+# ANOVA PARA MUSICAS1 (SAMPLE 1)
 anovaMusicas1 <- aov(danceability ~ tempo + country, data = musicas1)
 summary(anovaMusicas1)
 
@@ -38,7 +38,4 @@ summary(anovaMusicas1)
 anovaMusicas2 <- aov(danceability ~ tempo + country, data = musicas2)
 summary(anovaMusicas2)
 
-# ANOVA PARA MUSICAS (FULL SAMPLE)
-anovaMusicasFull <- aov(danceability ~ tempo + country, data = musicas)
-summary(anovaMusicasFull)
 
