c30e6901f2eab48f780bd3af3cf9c5e3dd512de4
Júlia Du Bois <jujubadubois@gmail.com>
2024-09-26 18:11:27 -0300

    Added valence to Welch ANOVA

--- codigos-R/anovaWelchDoisFatores.R
+++ codigos-R/anovaWelchDoisFatores.R
@@ -4,28 +4,25 @@ musicas <- read_csv("Documents/faculdade/Spotify_MQAM/tabelas/musicas_normalizad
 View(musicas)
 
 # FILTRANDO OS DADOS NÃO-PARAMETRIZÁVEIS
-musicasFiltrado <- musicas[musicas$country!="NG",]
+musicasFiltrado <- musicas[musicas$country!="NG" & musicas$country!="IN" & musicas$country!="US" & musicas$country!="MX" & musicas$country!="NZ" & musicas$country!="DE",]
 
 # AGRUPAR OS VALORES PARA TEMPO
 musicasFiltrado$tempo[musicasFiltrado$tempo<=0.33] <- 0.33
-musicasFiltrado$tempo[musicasFiltrado$tempo>0.33 & musicasFiltrado$tempo<=0.67] <- 0.67
+musicasFiltrado$tempo[musicasFiltrado$tempo>0.33 & musicasFiltrado$tempo<=0.67] <- 0.85
 musicasFiltrado$tempo[musicasFiltrado$tempo>0.67 & musicasFiltrado$tempo<=1] <- 1
-# musicas$tempo[musicas$tempo==1] <- 1 já está feito
+
+# AGRUPAR OS VALORES PARA VALÊNCIA
+musicasFiltrado$valence[musicasFiltrado$valence<=0.33] <- 0.33
+musicasFiltrado$valence[musicasFiltrado$valence>0.33 & musicasFiltrado$valence<=0.85] <- 0.85
+musicasFiltrado$valence[musicasFiltrado$valence>0.85 & musicasFiltrado$valence<=1] <- 1
 
 # DIVIDIR DATABASE EM DUAS (TESTE CRUZADO SUGERIDO PELO PROFESSOR)
-n <- 1100 # number of original database
+n <- 600 # number of original database
 select <- sample(c(TRUE, FALSE), n, replace=TRUE, prob=c(0.5, 0.5))
 musicas1 <- musicasFiltrado[select, ]
 musicas2 <- musicasFiltrado[!select, ]
 
 # ANOVA PARA MUSICAS (FULL SAMPLE)
-# utilizamos ANOVA de dois fatores, pois temos duas variaveis independentes
-# e uma variavel dependende
-# independentes: tempo, pais
-# dependente: dancabilidade
-
-# para modificar variaveis:
-# dependente fica antes do '~'
-# independendetes ficam depois, separadas por '+'
 anovaWelchMusicasFull <- oneway.test(danceability ~ tempo + country, data = musicasFiltrado)
 summary(anovaMusicasFull)
+
