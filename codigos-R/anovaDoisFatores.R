584d3991f90a1e23ca4e3a82c472cb3ce45175e0
lucas <lucaslyra47@gmail.com>
2024-09-26 19:15:25 -0300

    adiciona testes de levene e plots

--- codigos-R/anovaDoisFatores.R
+++ codigos-R/anovaDoisFatores.R
@@ -1,20 +1,20 @@
 # IMPORTAR DATABASE
 library(readr)
-musicas <- read_csv("Documents/faculdade/Spotify_MQAM/tabelas/musicas_normalizadas.csv")
+musicas <- read_csv("./MQAM/MQAM/tabelas/musicas_normalizadas.csv")
 View(musicas)
 
 # FILTRANDO OS DADOS NÃO-PARAMETRIZÁVEIS
 musicasFiltrado <- musicas[musicas$country!="NG" & musicas$country!="IN" & musicas$country!="US" & musicas$country!="MX" & musicas$country!="NZ" & musicas$country!="DE",]
 
 # AGRUPAR OS VALORES PARA TEMPO
-musicasFiltrado$tempo[musicasFiltrado$tempo<=0.33] <- 0.33
-musicasFiltrado$tempo[musicasFiltrado$tempo>0.33 & musicasFiltrado$tempo<=0.67] <- 0.85
-musicasFiltrado$tempo[musicasFiltrado$tempo>0.67 & musicasFiltrado$tempo<=1] <- 1
+musicasFiltrado$tempo[musicasFiltrado$tempo<=0.33] <- "0.33"
+musicasFiltrado$tempo[musicasFiltrado$tempo>0.33 & musicasFiltrado$tempo<=0.67] <- "0.85"
+musicasFiltrado$tempo[musicasFiltrado$tempo>0.67 & musicasFiltrado$tempo<=1] <- "1"
 
 # AGRUPAR OS VALORES PARA VALÊNCIA
-musicasFiltrado$valence[musicasFiltrado$valence<=0.33] <- 0.33
-musicasFiltrado$valence[musicasFiltrado$valence>0.33 & musicasFiltrado$valence<=0.85] <- 0.85
-musicasFiltrado$valence[musicasFiltrado$valence>0.85 & musicasFiltrado$valence<=1] <- 1
+musicasFiltrado$valence[musicasFiltrado$valence<=0.33] <- "0.33"
+musicasFiltrado$valence[musicasFiltrado$valence>0.33 & musicasFiltrado$valence<=0.85] <- "0.85"
+musicasFiltrado$valence[musicasFiltrado$valence>0.85 & musicasFiltrado$valence<=1] <- "1"
 
 # DIVIDIR DATABASE EM DUAS (TESTE CRUZADO SUGERIDO PELO PROFESSOR)
 n <- 600 # number of original database
@@ -46,5 +46,10 @@ summary(anovaMusicas2)
 # anovaMusicas2$coefficients
 
 par(mfcol=c(2,2))
-plot(residuals(anovaMusicasFull))
+plot(anovaMusicasFull)
+par(mfcol=c(1,1))
 
+library(car)
+leveneTest(danceability ~ country, data = musicasFiltrado)
+leveneTest(danceability ~ tempo, data = musicasFiltrado)
+leveneTest(danceability ~ valence, data = musicasFiltrado)
